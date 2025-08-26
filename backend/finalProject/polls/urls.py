997a311bc721b5e05392a65c7a2d0a194460f9b3
Ahmad Irshaid <145376048+AhmadIrsahaid@users.noreply.github.com>
2025-08-26 11:33:10 +0300

    add verify email feature

--- backend/finalProject/polls/urls.py
+++ backend/finalProject/polls/urls.py
@@ -34,8 +34,6 @@ urlpatterns = [
     path("article/categories/", AllCategoriesView.as_view() , name="all-categories"),
     path("article/categories/<int:pk>", ArticleAndCategoryListView.as_view() , name="all-categories-with-article"),
     re_path(r'^signup/$', ReaderSignUpView.as_view(), name='signup'),
-    # re_path(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
-    #     views.activate, name='activate'),
-    path('activate/<uidb64>/<token>/', views.activate, name='activate')
+    path('activate/<uidb64>/<token>/', UserActivateView.as_view(), name='activate')
 
 ]
--- backend/finalProject/polls/views.py
+++ backend/finalProject/polls/views.py
@@ -23,7 +23,7 @@ from django.template.loader import render_to_string
 from .tokens import account_activation_token
 from django.contrib.auth import get_user_model
 from django.core.mail import EmailMessage
-
+# from CorrectPythonPackage.token import *
 def home(request):
     return JsonResponse({
         'message': 'Welcome from Django!',
@@ -268,7 +268,6 @@ class AllCategoriesView(ListView):
 
 
     def get_context_data(self, **kwargs):
-
         ctx = super().get_context_data(**kwargs)
         category_article_count = {}
         all_categories = Category.objects.all()
@@ -287,43 +286,19 @@ class ArticleAndCategoryListView(ListView):
        obj = Article.objects.filter(category__id=self.kwargs["pk"])
        return obj
 
-def activate(request, uidb64, token):
-    try:
-        uid = force_str(urlsafe_base64_decode(uidb64))
-        User = get_user_model()
-        user = User.objects.get(pk=uid)
-    except(TypeError, ValueError, OverflowError, User.DoesNotExist):
-        user = None
-    if user is not None and account_activation_token.check_token(user, token):
-        user.is_active = True
-        user.save()
-        login(request, user)
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
-    else:
-        return HttpResponse('Activation link is invalid!')
-
-def signup(request):
-
-    if request.method == "POST":
-        form = ReaderSignUpForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit=False)
-            user.is_active = False
+class UserActivateView(View):
+    def get(self, request, *args, **kwargs):
+        try:
+            uid = force_str(urlsafe_base64_decode(kwargs.get('uidb64')))
+            UserModel = get_user_model()
+            user = UserModel.objects.get(pk=uid)
+        except(TypeError, ValueError, OverflowError):
+            user = None
+        if user is not None and account_activation_token.check_token(user, kwargs.get('token')):
+            user.is_active = True
             user.save()
-            current_site = get_current_site(request)
-            mail_subject = "Your account has been created"
-            message = render_to_string(
-                'registration/acc_active_email.html' , {
-                    'user': user,
-                    'domain': current_site.domain,
-                    'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-                    'token': account_activation_token.make_token(user),
-                }
-            )
-            to_email = form.cleaned_data.get('email')
-            email = EmailMessage(mail_subject, message, to=[to_email])
-            email.send()
-            return redirect('activate/<uidb64>/<token>/' )
-    else:
-        form = SignupForm()
-    return render(request, 'registration/signup.html', {'form': form})
+            login(self.request, user)
+            return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        else:
+            return HttpResponse('Activation link is invalid!')
+
